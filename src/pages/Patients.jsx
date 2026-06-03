503450da66642cc08baae657fdc7fc7d206650b7
getlumi <getlumi05@gmail.com>
2026-06-02 23:24:16 -0500

    Update Patients.jsx

--- src/pages/Patients.jsx
+++ src/pages/Patients.jsx
@@ -4,9 +4,9 @@ import jsPDF from 'jspdf'
 
 const HOURS = ['08:00','08:30','09:00','09:30','10:00','10:30','11:00','11:30','12:00','12:30','13:00','13:30','14:00','14:30','15:00','15:30','16:00','16:30','17:00','17:30','18:00']
 
-export default function Patients({
-  const localToday = () => new Intl.DateTimeFormat('en-CA', { timeZone: 'America/Cancun' }).format(new Date())
- ({ clinic, openNew }) {
+const localToday = () => new Intl.DateTimeFormat('en-CA', { timeZone: 'America/Cancun' }).format(new Date())
+
+export default function Patients({ clinic, openNew }) {
   const [tab, setTab]                       = useState('todos')
   const [lumiPatients, setLumiPatients]     = useState([])
   const [regularPatients, setRegularPatients] = useState([])
